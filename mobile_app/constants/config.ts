a3e96989589024d805d2befc8694ea360d09d51b
byiringiro-aloys <byiringiroaloys8@gmail.com>
2026-03-12 15:45:33 +0200

    Network access config on mobile

--- mobile_app/constants/config.ts
+++ mobile_app/constants/config.ts
@@ -3,5 +3,5 @@
  */
 
 // Use the VPS IP for shared backend or manual local IP if needed on a real device
-export const API_BASE_URL = 'http://10.12.74.59:8208';
-export const SOCKET_URL = 'http://10.12.74.59:8208';
+export const API_BASE_URL = 'http://157.173.101.159:8208';
+export const SOCKET_URL = 'http://157.173.101.159:8208';
